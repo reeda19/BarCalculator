bda7ea1820d17bc1b0dfa95043fe732abe11c3b9
Alex Reed <alexreed100@gmail.com>
2020-11-23 16:06:12 -0500

    added basic private helper methods to controller

--- src/controller/BarCalculatorController.java
+++ src/controller/BarCalculatorController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import java.util.Scanner;
 import model.IBarCalculatorModel;
 
 public class BarCalculatorController implements IBarCalculatorController {
@@ -23,10 +24,39 @@ public class BarCalculatorController implements IBarCalculatorController {
   /**
    * Takes in commands to add people, add drinks, and display results
    *
-   * @param rd Readable to read commands from
+   * @param scanner Readable to read commands from
    */
   @Override
-  public void startCalculator(Readable rd) {
+  public void startCalculator(Scanner scanner, IBarCalculatorModel model) {
 
+  while(scanner.hasNext()){
+    String next = scanner.next().strip().toLowerCase();
+    switch(next){
+      case "drink":
+        parseDrink(scanner);
+        break;
+      case "person":
+        parsePerson(scanner);
+        break;
+      case "addDrink":
+        parseAddDrink(scanner);
+        break;
+      default:
+        throw new IllegalArgumentException("Illegal commands to scanner");
+    }
+  }
+
+
+
+
+  }
+
+  private void parseAddDrink(Scanner scanner) {
+  }
+
+  private void parsePerson(Scanner scanner) {
+  }
+
+  private void parseDrink(Scanner scanner) {
   }
 }
--- src/controller/IBarCalculatorController.java
+++ src/controller/IBarCalculatorController.java
@@ -1,13 +1,18 @@
 package controller;
 
+import java.util.Scanner;
+import model.IBarCalculatorModel;
+
 public interface IBarCalculatorController {
 
 
   /**
    * Takes in commands to add people, add drinks, and display results
-   * @param rd Readable to read commands from
+   *
+   * @param scanner Readable to read commands from
+   * @param model model to use
    */
-  public void startCalculator(Readable rd);
+  public void startCalculator(Scanner scanner, IBarCalculatorModel model);
 
 
 }
