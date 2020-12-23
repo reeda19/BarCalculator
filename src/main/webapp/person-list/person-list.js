7c4c2edb04b8f712be3f54652518a7399fe3202e
Alex Reed <alexreed100@gmail.com>
2020-12-22 23:56:07 -0500

    implemented part of person-list

---
+++ src/main/webapp/person-list/person-list.js
@@ -0,0 +1,82 @@
+class PersonList extends React.Component {
+  state = {
+    persons: [],
+  }
+
+  findAllPersons = () =>
+      findAllPersons()
+      .then((persons) => this.setState({persons}))
+
+  createPerson = () =>
+      createPerson()
+      .then(() => this.findAllPersons())
+
+  deletePerson = (personId) =>
+      deletePerson(personId)
+      .then(() => this.findAllPersons())
+
+  componentDidMount = () =>
+      this.findAllPersons()
+
+  render() {
+    return (
+
+        <div className="container-fluid">
+          <button
+              className="btn btn-success float-right"
+              onClick={() => this.createPerson()}>
+            Create
+          </button>
+          <a className="btn btn-danger float-right"
+             href="../../index.html">
+            Home
+          </a>
+          <h1>Person List</h1>
+          <table className="table">
+            <thead>
+            <tr>
+              <th>Person ID</th>
+              <th>Title</th>
+              <th>Author</th>
+              <th>&nbsp;</th>
+            </tr>
+            </thead>
+            <tbody>
+            {
+              this.state.persons.map(person =>
+                  <tr key={person.personId}>
+                    <td>{person.personId}</td>
+                    <td>{person.name}</td>
+                    <td>{person.total}</td>
+                    <td>person.drinkList</td>
+                    <td>
+                      <button
+                          id="delete" className="btn btn-danger float-right"
+                          onClick={() => this.deletePerson(person.personId)}>
+                        Delete
+                      </button>
+
+                      <a id="edit" className="btn btn-primary float-right"
+                         href={`../../person-editor/person-editor.html?personId=${person.personId}`}>
+                        Edit
+                      </a>
+
+
+                    </td>
+                  </tr>
+              )
+
+            }
+            </tbody>
+          </table>
+        </div>
+
+    )
+  }
+}
+
+ReactDOM.render(
+    <PersonList/>,
+    document.getElementById('root')
+)
+
