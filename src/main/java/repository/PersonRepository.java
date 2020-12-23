5be0750a1662d1c5cb85f94eda60b16b68b09395
Alex Reed <alexreed100@gmail.com>
2020-12-22 23:44:05 -0500

    implemented PersonRepository

--- src/main/java/repository/PersonRepository.java
+++ src/main/java/repository/PersonRepository.java
@@ -1,5 +1,8 @@
 package repository;
 
-public class PersonRepository {
+import model.Person;
+import org.springframework.data.repository.CrudRepository;
+
+public class PersonRepository extends CrudRepository<Person, Integer> {
 
 }
